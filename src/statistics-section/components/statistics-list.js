27cb0f99302758616595de7a99b79dd7770ade20
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-10-03 09:00:38 +0300

    1.12

--- src/statistics-section/components/statistics-list.js
+++ src/statistics-section/components/statistics-list.js
@@ -1,24 +1,33 @@
-import StatisticElement from './statistics'
-import style from './statistics.module.css'
+import PropTypes from 'prop-types';
+import style from './statistics.module.css';
 
 
 export default function StatisticList({ statData }) {
 
-    return <section className={style.statistics}>
-        <h2 className={style.title}>Upload stats</h2>
-        <ul className={style.statList} >
-            {statData.map((el) =>
-                <li key={el.id} style={{
-                    backgroundColor: `rgb(${105 + 150 * Math.random()}, ${105 + 150 * Math.random()}, ${105 + 150 * Math.random()})`,
-                    outline: "solid 1px black"
-                }}>
-                    <StatisticElement
-                        label={el.label}
-                        percentage={el.percentage} />
-                </li>
-            )}
-        </ul>
-    </section >
+    return <>
+        (<section className={style.statistics}>
+            <h2 className={style.title}>Upload stats</h2>
+            <ul className={style.statList} >
+                {statData.map((el) =>
+                    <li key={el.id} style={{
+                        backgroundColor: `rgb(${105 + 150 * Math.random()}, ${105 + 150 * Math.random()}, ${105 + 150 * Math.random()})`,
+                        outline: "solid 1px black"
+                    }}>
+                        <span className={style.label}>{el.label}</span>
+                        <span className={style.percentage}>{el.percentage}</span>
+                    </li>
+                )}
+            </ul>
+        </section >)</>
+}
+
+StatisticList.propTypes = {
+    statData: PropTypes.arrayOf(
+        PropTypes.shape({
+            percentage: PropTypes.number.isRequired,
+            label: PropTypes.string.isRequired
+        })
+    )
 }
 
 
